8570dc443c45b0e7618ec47633b25856cd7d5d3d
ArthurQueiroz2005 <arthur.queiroz@academiaevoque.com.br>
2024-02-12 14:02:06 -0300

    adicionei o component do checkInHistoryTable

--- src/components/checkInHistoryCard/checkInHistoryCard.tsx
+++ src/components/checkInHistoryCard/checkInHistoryCard.tsx
@@ -1,11 +1,57 @@
-import '../../assets/styles/components/checkInHistoryCard/StyleCheckInHistoryCard.css';
+import '../../assets/styles/components/checkInHistoryCard/styleCheckInHistoryCard.css';
 
-const checkInHistoryCard = () =>{
+const CheckInHistoryCard = () => {
     return (
         <div className='checkInHistoryCard'>
-             <h1>Teste</h1>
+            <div>
+                <h1 className='title'>Histórico de Check-in's</h1>
+                <table>
+                    <thead>
+                        <tr className='table-header'>
+                            <th>ID</th>
+                            <th>Nome</th>
+                            <th>Data</th>
+                            <th>Hora</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>João</td>
+                            <td>2024-02-12</td>
+                            <td>10:00</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Maria</td>
+                            <td>2024-02-13</td>
+                            <td>15:30</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Carlos</td>
+                            <td>2024-02-14</td>
+                            <td>09:45</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>João</td>
+                            <td>2024-02-12</td>
+                            <td>10:00</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Maria</td>
+                            <td>2024-02-13</td>
+                            <td>15:30</td>
+                        </tr>
+                      
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     )
 }
 
-export default checkInHistoryCard;
+export default CheckInHistoryCard;
--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -2,10 +2,12 @@ import { Outlet } from "react-router-dom"
 import { Sidebar } from "../components/sidebar/sidebar";
 import HelloUserCard from "../components/helloUserCard/helloUserCard";  
 import Searchbar from "../components/searchbar/searchbar";
+import CheckInHistoryCard from "../components/checkInHistoryCard/checkInHistoryCard";
 export function Layout () {
 
     return (
         <>              
+           <CheckInHistoryCard />
             <HelloUserCard />
             <Sidebar/>
             <Searchbar />
