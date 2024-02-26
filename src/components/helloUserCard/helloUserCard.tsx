5d90dd8e33f2220a45ac24ee39b6fb1fb51f4191
ArthurQueiroz2005 <arthur.queiroz@academiaevoque.com.br>
2024-02-26 10:11:53 -0300

    mexi de novo nov hello user card pois ele estava muito grande

--- src/components/helloUserCard/helloUserCard.tsx
+++ src/components/helloUserCard/helloUserCard.tsx
@@ -4,7 +4,7 @@ import '../../assets/styles/components/helloUserCard/styleHelloUserCard.css'
 export function HelloUserCard () {
   return (
     <div className="hello-user-card">
-      <h3 className='hello-user-title'>Olá, Usuário</h3>
+      <h4 className='hello-user-title'>Olá, Usuário</h4>
         <section className='hello-user-content'>
           <p>Bem vindo a tela de checkin da evopass</p>    
           <img src={imgHelloUserCard} alt="icon svg user" className='hello-user-card-img' />
