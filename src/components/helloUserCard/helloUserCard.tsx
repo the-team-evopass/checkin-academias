7c310ecd234a52cd6496b73670ce7968b8565c68
ArthurQueiroz2005 <arthur.queiroz@academiaevoque.com.br>
2024-02-26 10:05:24 -0300

    dei uma mexida no hello user card que tinha se alterado

--- src/components/helloUserCard/helloUserCard.tsx
+++ src/components/helloUserCard/helloUserCard.tsx
@@ -4,7 +4,7 @@ import '../../assets/styles/components/helloUserCard/styleHelloUserCard.css'
 export function HelloUserCard () {
   return (
     <div className="hello-user-card">
-      <h1>Olá, Usuário</h1>
+      <h3 className='hello-user-title'>Olá, Usuário</h3>
         <section className='hello-user-content'>
           <p>Bem vindo a tela de checkin da evopass</p>    
           <img src={imgHelloUserCard} alt="icon svg user" className='hello-user-card-img' />
